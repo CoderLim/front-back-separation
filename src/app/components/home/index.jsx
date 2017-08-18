b5e8865fda4dcbb040e86baf451a510c26a6e76f
gengliming <gengliming@Lim-GengdeMacBook-Pro.local>
2017-08-18 18:22:11 +0800

    Api proxy

--- src/app/components/home/index.jsx
+++ src/app/components/home/index.jsx
@@ -1,7 +1,27 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import reqwest from 'reqwest';
 
 export default class extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    //
+    // Http request should not be placed in `constructor` method,
+    // Otherwise, you will encounter something wrong.
+    //
+    reqwest({
+      url: 'api/cmdb/server/search',
+      type: 'json'
+    }).then(result => {
+      console.log(result);
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
   render = () => <div>
     <h1>This is a Home Page.</h1>
     <Link to="/about">about</Link>
--- src/server.js
+++ src/server.js
@@ -8,14 +8,18 @@ import express from 'express';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router';
+import httpProxy from 'http-proxy-middleware';
 import App from './app/index.jsx';
 import template from './template';
 
 const server = express();
 
 server.use('/assets', express.static('assets'));
+server.use('/api', httpProxy({target: 'http://10.185.30.237:8095', changeOrigin: true, pathRewrite: {
+  '^/api': ''
+}}));
 
-server.get('*', (req, res) => {
+server.get('/', (req, res) => {
   let context = {};
   const html = renderToString(
      <StaticRouter context={context} location={req.url}>
@@ -23,8 +27,7 @@ server.get('*', (req, res) => {
      </StaticRouter>);
 
   res.send(template({
-    //
-    // Markup different on the server and the client
+    // Fixed: Markup different on the server and the client
     // https://github.com/Hashnode/mern-starter/issues/149
     body: process.env.NODE_ENV === 'production' ? html : `<div>${html}</div>`,
     title: 'first server rendering app'
